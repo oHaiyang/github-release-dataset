376f7516a622a263807497bed9cfc0bf00163d5e
Haiyang Peng <phycgp@live.com>
2018-05-11 07:43:34 +0800

    test: test for `getRelease`

--- test/smuggler.test.js
+++ test/smuggler.test.js
@@ -16,12 +16,16 @@ dataset:
 \`\`\`
 `;
 const _mockRelease = {
-  tag_name: '_mockRelease',
-  body: _mockReleaseNote,
+  meta: {},
+  data: {
+    tag_name: '_mockRelease',
+    body: _mockReleaseNote,
+  }
 };
 
 
 describe('getRelease', () => {
+
   it('should return cached release if exists', async () => {
     const s = new Smuggler();
     s.releases[_mockTagName] = _mockRelease;
@@ -29,4 +33,27 @@ describe('getRelease', () => {
     expect(await s.getRelease(_mockTagName)).toEqual(_mockRelease);
     expect(s.g.repos.getReleaseByTag.mock).toBeTruthy(); 
   });
+
+  it('should return get release and cache it and return', async () => {
+    const s = new Smuggler();
+    s.g.repos.getReleaseByTag.mockResolvedValue(_mockRelease);
+
+    const result = await s.getRelease(_mockTagName);
+    expect(s.g.repos.getReleaseByTag.mock.calls.length).toBe(1);
+    expect(s.g.repos.getReleaseByTag.mock.calls[0][0].tag).toBe(_mockTagName);
+    expect(result).toEqual(_mockRelease.data);
+    expect(s.releases[_mockTagName]).toEqual(_mockRelease.data);
+  });
+
+  it('should re throw an error when get remote relase failed', async () => {
+    const s = new Smuggler();
+    s.g.repos.getReleaseByTag.mockReturnValueOnce();
+
+    try {
+      await s.getRelease(_mockTagName);
+    } catch (e) {
+      expect(e.message).toMatch(new RegExp(`Failed to get release ${_mockTagName}. `)) 
+    }
+  });
+
 });
