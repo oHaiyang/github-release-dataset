71f777ae5b85b1fa9ccb5f53fa4f9b1946cc7195
Haiyang Peng <phycgp@live.com>
2018-05-10 16:49:28 +0800

    chore: add eslint

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,13 +1,16 @@
+/* eslint-env jest */
 import { buildCodeBlock, COMMENT } from '../src/utils';
 
 describe('buildCodeBlock', () => {
   it('should return a yaml block', () => {
     const _text = '---';
+
     const _block = `\`\`\`yaml
 ${COMMENT}
 
 ---
-\`\`\``
+\`\`\``;
+
     expect(buildCodeBlock(_text)).toBe(_block);
   });
 });
