4d8708178a40294c6b27fb028ef6292efcae8234
Haiyang Peng <phycgp@live.com>
2018-05-10 14:19:17 +0800

    test: initiate and add the first test

---
+++ test/utils.test.js
@@ -0,0 +1,13 @@
+import { buildCodeBlock, COMMENT } from '../src/utils';
+
+describe('buildCodeBlock', () => {
+  it('should return a yaml block', () => {
+    const _text = '---';
+    const _block = `\`\`\`yaml
+${COMMENT}
+
+---
+\`\`\``
+    expect(buildCodeBlock(_text)).toBe(_block);
+  });
+});
