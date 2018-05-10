412ab4cbd2b3812199d2034359e9391033e1c226
Haiyang Peng <phycgp@live.com>
2018-05-10 17:07:30 +0800

    test: add tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,7 +1,35 @@
 /* eslint-env jest */
-import { buildCodeBlock, COMMENT } from '../src/utils';
+import { buildCodeBlock, buildDatasetObj, readDatasets, COMMENT, DATASET_KEY } from '../src/utils';
+
+const _jerryDatasetName = '_jerry';
+const _tomDatasetName = '_tom';
+const _mockNote = 
+`
+this is a test line.
+
+\`\`\`yaml
+this is not a dataset
+\`\`\`
+
+\`\`\`yaml
+dataset-name: _jerry
+dataset:
+  _foe: _tom
+\`\`\`
+
+yohohoho
+
+\`\`\`yaml
+dataset-name: _tom
+dataset:
+  _food: _jerry
+\`\`\`
+
+this is another test line
+`;
 
 describe('buildCodeBlock', () => {
+
   it('should return a yaml block', () => {
     const _text = '---';
 
@@ -12,5 +40,33 @@ ${COMMENT}
 \`\`\``;
 
     expect(buildCodeBlock(_text)).toBe(_block);
+  });
+
+});
+
+describe('buildDatasetObj', () => {
+
+  it('should consturct a dataset object', () => {
+    const _name = '_jerry';
+    const _dataset = {
+      _foe: '_tom',
+    };
+
+    expect(buildDatasetObj(_name, _dataset)).toEqual({
+      [DATASET_KEY]: _name,
+      dataset: _dataset,
+    });
+  });
+
+});
+
+describe('readDatasets', () => {
+
+  it('should read all datasets when datasetName not provided', () => {
+    expect(readDatasets(_mockNote)).toMatchSnapshot();
+  });
+
+  it('should read datasets have specified name', () => {
+    expect(readDatasets(_mockNote, _jerryDatasetName)).toMatchSnapshot();
   });
 });
